8de7cbdfd5d0abc60460d79df6b08006b1890f79
geeknr1 <146103854+geeknr1@users.noreply.github.com>
2024-06-18 23:05:38 +0300

    Add files via upload

---
+++ backToTop.js
@@ -0,0 +1,15 @@
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
